6265bb0913df5b53c5776e629870b4292d4204e6
Radoje <megasrbin1@gmail.com>
2023-04-08 00:08:50 +0200

    add prettier config with couple of custom components that will be using later, add tailwind

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,19 @@
+import { Github } from "lucide-react"
+import { Button } from "./ui/button"
+
+export default function MainHeader() {
+	return (
+		<header className="flex items-center justify-between px-36">
+			<h1 className=""> Logo </h1>
+			<nav>
+				<ul>
+					<li>
+						<Button>
+							<Github className="mr-3 aspect-square w-4" /> Get in Boy
+						</Button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
